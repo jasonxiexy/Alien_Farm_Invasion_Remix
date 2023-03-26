8529a86d2735a6fa59aac5cb87d990f1d9886d7d
Jason Xie <60050513+jasonxiexy@users.noreply.github.com>
2023-03-26 19:59:39 -0400

    Update DoorFinish.cs

--- Assets/Jason/DoorFinish.cs
+++ Assets/Jason/DoorFinish.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Door : MonoBehaviour
+public class DoorFinish : MonoBehaviour
 {
 
     private PlayerMovement thePlayer;
