bf7d93e3024896a27c88e96cd2db5ea591c8fbcf
jianing410 <106984102+jianing410@users.noreply.github.com>
2023-03-26 16:26:46 -0400

    level 3 + shooting skill
    3.26

--- Assets/Scripts/falling_stone.cs
+++ Assets/Scripts/falling_stone.cs
@@ -6,18 +6,19 @@ public class falling_stone : MonoBehaviour
 {
     public float speed = 9.98f;
     public float yLimit = -12.0f;
-    public float gravity = 9.8f;
+    // Start is called before the first frame update
 
+
+    // Update is called once per frame
     void Update()
     {
         Vector3 targetPosition = transform.position;
-        targetPosition.y -= 10f * Time.deltaTime; // Replace 10f with the constant speed you want the stone to fall at
+        targetPosition.y += -10f;
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-
-        if (transform.position.y < yLimit)
-        {
+        if(transform.position.y < yLimit){
             Destroy(gameObject);
             transform.parent.GetComponent<StoneManager>().SpawnStone();
         }
     }
+
 }
