ed4f98bb2288172bceb188f8f48353f26bfbb5bf
Jason Xie <60050513+jasonxiexy@users.noreply.github.com>
2023-03-27 21:47:02 -0400

    resolve merge conflict

--- Assets/Scripts/Jumper.cs
+++ Assets/Scripts/Jumper.cs
@@ -8,7 +8,7 @@ public class Jumper : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("player"))
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
         }
