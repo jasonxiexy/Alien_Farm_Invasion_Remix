416d4b6a842407dd4192ae74fade4f0420dc9506
Deckard-YYZ <435341343@qq.com>
2023-04-02 17:04:09 -0400

    Reborn position and restart

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -7,6 +7,7 @@ public class PlayerLife : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+    public PlayerHealth health;
 
     [SerializeField] AudioSource deathSoundEffect;
 
@@ -22,17 +23,34 @@ public class PlayerLife : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("trap")){
             Die();
+            StartCoroutine(WaitAndRespawn(collision));
+            
+            // Vector3   rebornP = collision.transform.parent.GetChild(0).transform.position;
+            // Debug.Log("Parent function called");
+            // transform.position = rebornP;
+            // health.damage();
         }
-        //else if (rb.velocity.y < -5)
-        //{
-        //    Die();
-        //}
     }
+
+    private IEnumerator WaitAndRespawn(Collision2D collision)
+{
+    // Wait for 1 second
+    if (health.currentHealth == 0) RestartLevel();
+    yield return new WaitForSeconds(1f);
+
+    // Get the respawn point from the parent object
+    Vector3 respawnPoint = collision.transform.GetChild(0).transform.position;
+    Debug.Log("Parent function called");
+    transform.position = respawnPoint;
+
+    // Damage the player's health
+    health.damage();
+}
 
     private void Die()
     {
         deathSoundEffect.Play();
-        rb.bodyType = RigidbodyType2D.Static;
+        //rb.bodyType = RigidbodyType2D.Static;
         anim.SetTrigger("Death");
     }
 
