1f87ee5949fc1aba112d1b69fb029419413704ad
Jason Xie <60050513+jasonxiexy@users.noreply.github.com>
2023-04-02 21:51:11 -0400

    boss

--- Assets/Maria/Bullet.cs
+++ Assets/Maria/Bullet.cs
@@ -7,11 +7,26 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
+    public int damage = 40;
+    //public GameObject impactEffect;
     
     void Start()
     {
         rb.velocity = transform.right * speed;
     }
+
+    //void OnTriggerEnter2D(Collider2D hitInfo)
+    //{
+    //    Boss_Health enemy = hitInfo.GetComponent<Boss_Health>();
+    //    if (enemy != null)
+    //    {
+    //        enemy.TakeDamage(damage);
+    //    }
+
+    //    Instantiate(impactEffect, transform.position, transform.rotation);
+
+    //    Destroy(gameObject);
+    //}
 
 
 }
